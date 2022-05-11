cbdfc7c50fc89a5988e67b7e21b9cef9593a07ce
A.YOGUN <yogun@student.42heilbronn.de>
2022-05-11 12:10:54 +0200

    Update solution.java

--- NumberSearch/solution.java
+++ NumberSearch/solution.java
@@ -1,1 +1,39 @@
-Have the function NumberSearch(str) take the str parameter, search for all the numbers in the string, add them together, then return that final number divided by the total amount of letters in the string. For example: if str is "Hello6 9World 2, Nic8e D7ay!" the output should be 2. First if you add up all the numbers, 6 + 9 + 2 + 8 + 7 you get 32. Then there are 17 letters in the string. 32 / 17 = 1.882, and the final answer should be rounded to the nearest whole number, so the answer is 2. Only single digit numbers separated by spaces will be used throughout the whole string (So this won't ever be the case: hello44444 world). Each string will also have at least one letter.
+import java.util.*; 
+import java.io.*;
+
+class Main {
+
+  public static int SearchingChallenge(String str) {
+    // code goes here
+    int number;
+    int result;
+    int strLen;
+    int letter;
+
+    strLen = str.length();
+    number = 0;
+    letter = 0;
+    for(int i = 0 ; i < strLen ; i++)
+    {
+        if(Character.isDigit(str.charAt(i)))
+        {
+          //System.out.print("I am digit in string:" + str.charAt(i) + "\n");
+          number = number + (str.charAt(i) - '0');
+        }       
+        else if(Character.isLetter(str.charAt(i)))
+        {
+          letter = letter + 1;
+        }
+    } 
+    //System.out.print("this is number:" + number + "\n");
+    result = (int)Math.rint(number/letter);
+    return result;
+  }
+
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner s = new Scanner(System.in);
+    System.out.print(SearchingChallenge(s.nextLine())); 
+  }
+
+}
